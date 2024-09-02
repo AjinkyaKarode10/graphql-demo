539c1c8eeb7b09a60eccd38669d4c4613f3ec51e
AjinkyaKarode10 <akarode199@gmail.com>
2024-09-02 13:02:10 +0530

    Adding Command-Query pattern

--- src/main/java/com/dailycodebuffer/graphqldemo/controller/PlayerController.java
+++ src/main/java/com/dailycodebuffer/graphqldemo/controller/PlayerController.java
@@ -1,9 +1,9 @@
 package com.dailycodebuffer.graphqldemo.controller;
 
+import com.dailycodebuffer.graphqldemo.command.PlayerCommandServiceImpl;
+import com.dailycodebuffer.graphqldemo.query.PlayerQueryServiceImpl;
 import com.dailycodebuffer.graphqldemo.model.Player;
-import com.dailycodebuffer.graphqldemo.model.PlayerRecord;
 import com.dailycodebuffer.graphqldemo.model.Team;
-import com.dailycodebuffer.graphqldemo.service.PlayerService;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -14,30 +14,38 @@ import java.util.List;
 @Controller
 public class PlayerController {
 
-    private final PlayerService playerService;
+//    private final PlayerService playerService;
+//
+//    public PlayerController(PlayerService playerService) {
+//        this.playerService = playerService;
+//    }
 
-    public PlayerController(PlayerService playerService) {
-        this.playerService = playerService;
+    private final PlayerCommandServiceImpl playerCommandServiceImpl;
+    private final PlayerQueryServiceImpl playerQueryServiceImpl;
+
+    public PlayerController(PlayerCommandServiceImpl playerCommandServiceImpl, PlayerQueryServiceImpl playerQueryServiceImpl) {
+        this.playerCommandServiceImpl = playerCommandServiceImpl;
+        this.playerQueryServiceImpl = playerQueryServiceImpl;
     }
 
     @QueryMapping
     public List<Player> findAll() {
-        return playerService.findAll();
+        return playerQueryServiceImpl.findAllPLayers();
     }
 
     @QueryMapping
     public Player findOne(@Argument String playerId) {
-        return playerService.findOne(playerId);
+        return playerQueryServiceImpl.findByPlayerId(playerId);
     }
 
     @MutationMapping
     public Player create(@Argument String playerId, @Argument String name, @Argument Team team) {
-        return playerService.create(playerId,name,team);
+        return playerCommandServiceImpl.createPlayer(playerId,name,team);
     }
 
     @MutationMapping
     public Player update(@Argument String playerId, @Argument String name, @Argument Team team) {
-        return playerService.update(playerId,name,team);
+        return playerCommandServiceImpl.updatePlayer(playerId,name,team);
     }
 
 //    @MutationMapping
